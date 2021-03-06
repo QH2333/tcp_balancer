06bc7abc8ba798f28dfcc6642e27cb375142872b
QH2333 <qi_an_hao@126.com>
2021-03-06 19:55:40 +0800

    format code

--- socket_client.cpp
+++ socket_client.cpp
@@ -15,22 +15,25 @@ constexpr uint16_t SERVER_PORT = 2333;
 constexpr auto MAX_CONCURRENCY = 10;
 constexpr bool IS_SERVER = false;
 
+static pthread_mutex_t output_mutex;
+
 void cleanup(int);
 void *tcp_client_thread(void *arg);
-inline int find_next_available(bool *tcp_conn_bitmap);
-
-tcp_status_t tcp_status_list[MAX_CONCURRENCY];
-pthread_t thread_id[MAX_CONCURRENCY];
-tcp_client_arg thread_arguments_list[MAX_CONCURRENCY];
 
 int main()
 {
     signal(SIGINT, cleanup);
 
+    client_status_t client_status; // Status of the client
+    tcp_status_t tcp_status_list[MAX_CONCURRENCY]; // Status per TCP connection
+    pthread_t thread_id[MAX_CONCURRENCY];
+
     for (int id = 0; id < MAX_CONCURRENCY; id++)
     {
-        thread_arguments_list[id] = (tcp_client_arg){id, 0, tcp_status_list + id};
-        pthread_create(&thread_id[id], nullptr, tcp_client_thread, &thread_arguments_list[id]);
+        tcp_status_list[id].tcp_conn_id = id;
+        tcp_status_list[id].tcp_conn_fd = 0;
+        tcp_status_list[id].status_info = &client_status;
+        pthread_create(&thread_id[id], nullptr, tcp_client_thread, tcp_status_list + id);
     }
 
     for (int id = 0; id < MAX_CONCURRENCY; id++)
@@ -50,8 +53,8 @@ void cleanup(int)
 void *tcp_client_thread(void *arg)
 {
     // Unpackage arguments
-    int thread_id = ((tcp_client_arg *)arg)->thread_id;
-    tcp_status_t *tcp_status = ((tcp_client_arg *)arg)->tcp_status;
+    tcp_status_t *tcp_status = (tcp_status_t *)arg;
+    client_status_t *client_status = (client_status_t *)tcp_status->status_info;
 
     // Create socket
     int tcp_client_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -78,12 +81,12 @@ void *tcp_client_thread(void *arg)
     sockaddr_in tcp_local_addr;
     getsockname(tcp_client_fd, (sockaddr *)&tcp_local_addr, &tcp_addr_len);
     getpeername(tcp_client_fd, (sockaddr *)&tcp_peer_addr, &tcp_addr_len);
-    tcp_status->tcp_tuple_info.c_server_ip = ntohl(tcp_local_addr.sin_addr.s_addr);
-    tcp_status->tcp_tuple_info.c_server_port = ntohs(tcp_local_addr.sin_port);
-    tcp_status->tcp_tuple_info.c_client_ip = ntohl(tcp_peer_addr.sin_addr.s_addr);
-    tcp_status->tcp_tuple_info.c_client_port = ntohs(tcp_peer_addr.sin_port);
+    tcp_status->tcp_tuple_info.c_server_ip = ntohl(tcp_peer_addr.sin_addr.s_addr);
+    tcp_status->tcp_tuple_info.c_server_port = ntohs(tcp_peer_addr.sin_port);
+    tcp_status->tcp_tuple_info.c_client_ip = ntohl(tcp_local_addr.sin_addr.s_addr);
+    tcp_status->tcp_tuple_info.c_client_port = ntohs(tcp_local_addr.sin_port);
 
-    std::cout << format_tcp_tuple(tcp_status->tcp_tuple_info, SERVER_ALL) << std::endl;
+    // std::cout << format_tcp_tuple(tcp_status->tcp_tuple_info, SERVER_ALL) << std::endl;
     std::cout << format_tcp_tuple(tcp_status->tcp_tuple_info, CLIENT_ALL) << std::endl;
 
     // char msg[1024];
